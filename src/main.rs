4fdc85bdec5a127781571a5f122498da9c1c9b82
Sam <samhalpern58@gmail.com>
2021-11-10 00:07:21 -0600

    other typos

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 #![no_main]
 #![no_std]
 
-use panic_semihosting as _;
+use panic_halt as _;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use stm32f1xx_hal::prelude::*;
-use stm32f1xx_hal::stm32;
+use stm32f4xx_hal::prelude::*;
+use stm32f4xx_hal::stm32;
 
 #[entry]
 fn main() -> ! {
     let _peripherals = cortex_m::Peripherals::take().unwrap();
     let _device = stm32::Peripherals::take().unwrap();
 
-    hprintln!("Hello semihosting world");
+    hprintln!("Hello semihosting world").unwrap();
     let mut x = 0;
     loop {
         x += 1;
-        hprintln!("{} loop iteration{}", x, if x > 1 { "s" } else { "" })
+        hprintln!("{} loop iteration{}", x, if x > 1 { "s" } else { "" }).unwrap();
     }
 }
